32b08e08256d327b8b286b062b7b0c7a753f9094
Karidus-423 <kapuerto23@gmail.com>
2025-06-06 19:12:11 +0000

    playing around

--- src/update.c
+++ src/update.c
@@ -2,7 +2,10 @@
 #include <SDL3/SDL_pixels.h>
 #include <SDL3/SDL_rect.h>
 #include <SDL3/SDL_render.h>
+#include <SDL3/SDL_stdinc.h>
+#include <SDL3/SDL_timer.h>
 #include <SDL3/SDL_video.h>
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -22,13 +25,21 @@ typedef struct {
 
 void UpdateApp(AppState *app) {
   Buffer bfr;
+  Uint64 tick = SDL_GetTicks() / 300;
+  long tick_x = (cos(tick) + 1) * 100;
+  long tick_y = (sin(tick) + 1) * 100;
+
+  if (tick > 255) {
+    tick = 0;
+  }
+
   bfr.pitch = app->width * sizeof(Pixel);
 
   bfr.pixels = malloc(sizeof(Pixel) * (app->height) * (app->width));
   for (int y = 0; y < app->height; y++) {
     for (int x = 0; x < app->width; x++) {
-      unsigned char r = x * 255 / (app->width - 1);
-      unsigned char b = y * 255 / (app->height - 1);
+      unsigned char r = (x * tick_x) / (app->width - 1);
+      unsigned char b = (y * tick_y) / (app->height - 1);
       bfr.pixels[(y * app->width) + x] = (Pixel){
           .a = 255,
           .b = b,
